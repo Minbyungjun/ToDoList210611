20288e39e82dfe4f8e7cd39e38035486d815344e
Minbyungjun <ladyfirst65@gmai.com>
2021-06-12 11:07:14 +0900

    rev2(6/12)

--- weather.js
+++ weather.js
@@ -2,26 +2,24 @@ const weather = document.querySelector(".js-weather");
 
 const API_KEY = "fb9beed6870878652b4a4065e7ffa7b1";
 const COORDS = 'coords';
-
-
-function getWeather(lat, lng){
+function getWeather(lat, lng) {
     fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`)
-    .then(function(response){
-       return response.json();
-    }).then(function(json){
-       const temperature = json.main.temp;
-       const place = json.name;
-       weather.innerText = `${temperature} @ ${place}`;
-    });
+        .then(function (response) {
+            return response.json();
+        }).then(function (json) {
+            const temperature = json.main.temp;
+            const place = json.name;
+            weather.innerText = `${temperature} @ ${place}`;
+        });
 }
 
 
 
-function saveCoords(coordsObj){
+function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
 
-function handleGeoSuccess(position){
+function handleGeoSuccess(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
     const coordsObj = {
@@ -32,27 +30,27 @@ function handleGeoSuccess(position){
     getWeather(latitude, longitude);
 }
 
-function handleGeoError(){
+function handleGeoError() {
     console.log("Can't access geo loacation");
 }
 
-function askForCoords(){
+function askForCoords() {
     //navigator API 호출 (https://openweathermap.org/)
     navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
 }
 
-function loadCoords(){
+function loadCoords() {
     const loadCoords = localStorage.getItem(COORDS);
-    if(loadCoords === null){
+    if (loadCoords === null) {
         askForCoords();
-    }else{
+    } else {
         //getWeaher
         const parseCoords = JSON.parse(loadCoords);
         getWeather(parseCoords.latitude, parseCoords.longitude);
     }
 }
 
-function init(){
+function init() {
     loadCoords();
 }
 
